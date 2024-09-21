65b4f250162cb8b0d09b5d5c483e9f3c0e0ae5d1
AminMusah <ameenmusah77@gmail.com>
2024-09-21 06:09:52 +0000

    add explore page

---
+++ app/(routes)/explore/page.tsx
@@ -0,0 +1,482 @@
+export default function Explore() {
+  return (
+    <div>
+      <div className="pt-32 flex justify-center items-center flex-col gap-y-6">
+        <div className="relative ">
+          <p className="font-thin md:text-5xl text-center mb-4">
+            KUSSA SHEA BLISS PRODUCTS
+          </p>
+          <span className="absolute bottom-[-8px] left-1/2 transform -translate-x-1/2  w-1/2 h-[2px] bg-black transition-all duration-300 ease-in-out"></span>
+        </div>
+        <p className="text-xs md:text-xl font-thin text-center max-w-xl py-16">
+          TYPES OF PRODUCT AND USES/BENEFITS
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1200px] md:h-[700px]">
+        <div
+          className="relative rounded-xl overflow-hidden group mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div
+          className=" rounded-xl overflow-hidden group h-full"
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              LEMON INFUSED SHEA BUTTER
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  Our lemon infused shea butter product is solely natural shea
+                  butter infused with lemon essential oil.
+                </p>
+              </div>
+              <div className="flex flex-col py-8">
+                <p className=" text-xs sm:text-xl md:text-base">
+                  The composition of the product is 90% shea butter and 10%
+                  lemon essential oil. The outcome of the product is solid.
+                  Below are some uses and benefits of our lemon infused shea
+                  butter.
+                </p>
+              </div>
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • Reduces anxiety and depression.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Eases morning sickness.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Improves skin health.
+                  </li>
+                  <li className="text-xs md:text-base">• Relieves pain.</li>
+                  <li className="text-xs md:text-base">
+                    • Improves cold symptoms.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Used in the treatment of acne.
+                  </li>
+                  <li className="text-xs md:text-base">• Heals wounds.</li>
+                  <li className="text-xs md:text-base">• Heals wounds.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className=" rounded-xl overflow-hidden group h-full "
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              COCONUT INFUSED SHEA BUTTER
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 90% shea butter and 10%
+                  Coconut essential oil. The outcome of the product is solid.
+                  Below are some uses and benefits of our Coconut infused shea
+                  butter.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • Reduces anxiety and depression.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Eases morning sickness.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Improves skin health.
+                  </li>
+                  <li className="text-xs md:text-base">• Relieves pain.</li>
+                  <li className="text-xs md:text-base">
+                    • Improves cold symptoms.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Used in the treatment of acne.
+                  </li>
+                  <li className="text-xs md:text-base">• Heals wounds.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="relative rounded-xl overflow-hidden group col-span-1  mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className="relative rounded-xl overflow-hidden group mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+        <div
+          className=" rounded-xl overflow-hidden group h-full"
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              CITRONELLA INFUSED SHEA BUTTER
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 90% shea butter and 10%
+                  Citronella essential oil. The outcome of the product is solid.
+                  Below are some uses and benefits of our Citronella infused
+                  shea butter.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • Prevents the growth or spread of harmful airborne
+                    bacteria.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Serves as a repellent for flying insects such as
+                    mosquitoes, etc.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Uplifts negative moods and relaxes the body and mind.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • It is reputed to reduce muscles spasms, ease headaches,
+                    and boost energy.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className=" rounded-xl overflow-hidden group h-full "
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              PERFUME INFUSED SHEA BUTTER
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 90% shea butter and 10%
+                  Perfume oil. The outcome of the product is solid. Below are
+                  some uses and benefits of our perfume infused shea butter.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • It gives a pleasant and desirable scent to one’s body.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Increases self-appeal and self-confidence.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Enhances health and well-being by improving one’s mood,
+                    reducing anxiety and stress.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Increasing cognitive function and improving sleep.{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="relative rounded-xl overflow-hidden group  mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className="relative rounded-xl overflow-hidden group  mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+        <div
+          className=" rounded-xl overflow-hidden group h-full "
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              LAVENDER INFUSED SHEA BUTTER
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 90% shea butter and 10%
+                  Perfume oil. The outcome of the product is solid. Below are
+                  some uses and benefits of our Lavender infused shea butter.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • Reduces anxiety and emotions
+                  </li>
+                  <li className="text-xs md:text-base">• Improves Sleep. </li>
+                  <li className="text-xs md:text-base">
+                    • Helps heal burns and wounds.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Restores completion.
+                  </li>
+                  <li className="text-xs md:text-base">• Reduces acne. </li>
+                  <li className="text-xs md:text-base">• Prevents wrinkles.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className=" rounded-xl overflow-hidden group h-full "
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              NATURAL SHEA BUTTER
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 100% shea butter. The
+                  outcome of the product is solid. Below are some uses and
+                  benefits of our Natural shea butter.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">• Reduces acne.</li>
+                  <li className="text-xs md:text-base">
+                    • Used in treating burns and cuts.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Cures dandruff in hair.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Prevents dry skin.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Also used for cooking.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="relative rounded-xl overflow-hidden group  mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className="relative rounded-xl overflow-hidden group col-span-1  mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+        <div
+          className=" rounded-xl overflow-hidden group h-full col-span-2"
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              VIRGIN COCONUT OIL
+            </h2>
+
+            <div className="flex flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 100% coconut oil. The
+                  outcome of the product is liquid. Below are some uses and
+                  benefits of our Coconut oil.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • Healthy for cooking.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Promotes hair growth and prevents hair damage.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Relieves skin irritation.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Moisturizes skin and promotes healthy skin as it prevents
+                    infections.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Serves as a natural deodorant.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Heals damaged cuticles.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    {" "}
+                    • Soothes chapped lips.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 p-2 h-[1500px] md:h-[700px] md:mt-44">
+        <div
+          className=" rounded-xl overflow-hidden group h-full"
+          // style={{
+          //   backgroundImage: "url('/images/IMG_9458.jpg')",
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
+        >
+          <div className="flex flex-col w-full h-full">
+            <h2 className="md:text-3xl font-bold transition-transform duration-300 w-full px-2 md:px-8">
+              LUXIE SHOWER GEL (AFRICAN BLACK SOAP)
+            </h2>
+
+            <div className="flex  flex-col w-full p-2 md:p-8 pt-12">
+              <div className="flex flex-col">
+                <p className="text-xs md:text-base">
+                  The composition of the product is 30% African black soap, 60%
+                  water, 5% vitamin E oil and 5 % lemon grass oil. The outcome
+                  of the product is liquid. Below are some uses and benefits of
+                  our shower gel.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <ul>
+                  <li className="text-xs md:text-base">
+                    • Promotes healthy skin.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Prevents skin itching.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Prevents fungal infections.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Minimizes appearance of scars.
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Has antibacterial properties.{" "}
+                  </li>
+                  <li className="text-xs md:text-base">
+                    • Reduces appearance of dark spots.{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="relative rounded-xl overflow-hidden group mb-2 md:mb-0"
+          style={{
+            backgroundImage: "url('/images/IMG_9330.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content */}
+        </div>
+      </div>
+    </div>
+  );
+}
