111eb08e3cfd69530b2a4a38461652bc62a76cee
AminMusah <ameenmusah77@gmail.com>
2024-09-22 23:04:16 +0000

    remove description from dashboard

--- app/(routes)/auth/dashboard/page.tsx
+++ app/(routes)/auth/dashboard/page.tsx
@@ -34,9 +34,6 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { ModeToggle } from "@/components/mode-toggle";
 import ProductsTable from "@/components/products-table";
 
-export const description =
-  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.";
-
 export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
