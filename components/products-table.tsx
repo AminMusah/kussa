c5c75f60f81cf699ea69bd97e3db0c95456da1e0
AminMusah <ameenmusah77@gmail.com>
2024-09-22 22:58:11 +0000

    add images to table

--- components/products-table.tsx
+++ components/products-table.tsx
@@ -64,7 +64,7 @@ export default function ProductsTable() {
                   alt="Product image"
                   className="aspect-square rounded-md object-cover"
                   height="64"
-                  src="/placeholder.svg"
+                  src="/images/IMG_9363.jpg"
                   width="64"
                 />
               </TableCell>
@@ -101,7 +101,7 @@ export default function ProductsTable() {
                   alt="Product image"
                   className="aspect-square rounded-md object-cover"
                   height="64"
-                  src="/placeholder.svg"
+                  src="/images/IMG_9363.jpg"
                   width="64"
                 />
               </TableCell>
@@ -138,7 +138,7 @@ export default function ProductsTable() {
                   alt="Product image"
                   className="aspect-square rounded-md object-cover"
                   height="64"
-                  src="/placeholder.svg"
+                  src="/images/IMG_9363.jpg"
                   width="64"
                 />
               </TableCell>
@@ -173,7 +173,7 @@ export default function ProductsTable() {
                   alt="Product image"
                   className="aspect-square rounded-md object-cover"
                   height="64"
-                  src="/placeholder.svg"
+                  src="/images/IMG_9363.jpg"
                   width="64"
                 />
               </TableCell>
@@ -210,7 +210,7 @@ export default function ProductsTable() {
                   alt="Product image"
                   className="aspect-square rounded-md object-cover"
                   height="64"
-                  src="/placeholder.svg"
+                  src="/images/IMG_9363.jpg"
                   width="64"
                 />
               </TableCell>
@@ -247,7 +247,7 @@ export default function ProductsTable() {
                   alt="Product image"
                   className="aspect-square rounded-md object-cover"
                   height="64"
-                  src="/placeholder.svg"
+                  src="/images/IMG_9363.jpg"
                   width="64"
                 />
               </TableCell>
