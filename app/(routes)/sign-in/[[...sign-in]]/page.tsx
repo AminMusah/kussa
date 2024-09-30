521a26d2dd99076a20b28f42e1d5e56956a01eca
AminMusah <ameenmusah77@gmail.com>
2024-09-30 21:26:32 +0000

    change login url navigation

--- app/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -33,7 +33,7 @@ export default function SignInForm() {
       // and redirect the user
       if (signInAttempt.status === "complete") {
         await setActive({ session: signInAttempt.createdSessionId });
-        router.push("/auth/dashboard/products");
+        router.push("/auth/dashboard");
       } else {
         // If the status is not complete, check why. User may need to
         // complete further steps.
