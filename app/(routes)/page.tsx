24fc924baaf65f8d9d78ec497a036f0a8a98687e
aminmusah <ameenmusah77@gmail.com>
2024-10-11 21:38:27 +0100

    add skeleton loader to homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/accordion";
 import useAllProducts from "@/hooks/use-all-products";
 import { useEffect } from "react";
+import { SkeletonCard } from "@/components/product-skeleton-card";
 
 export default function Home() {
   const router = useRouter();
@@ -59,21 +60,23 @@ export default function Home() {
       </div>
 
       <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-4 gap-8">
-        {products.slice(0, 4).map(
-          (
-            product: {
-              _id: string;
-              name: string;
-              description: string;
-              images: [];
-              link: string;
-              price: number;
-            },
-            index
-          ) => (
-            <ProductCard {...product} key={product._id} />
-          )
-        )}
+        {loading
+          ? Array.from({ length: 4 }, (_, index) => (
+              <SkeletonCard key={index} />
+            ))
+          : products.slice(0, 4).map(
+              (
+                product: {
+                  _id: string;
+                  name: string;
+                  description: string;
+                  images: [];
+                  link: string;
+                  price: number;
+                },
+                index
+              ) => <ProductCard {...product} key={product._id} />
+            )}
       </div>
       <div className="group/wrap relative explore">
         <div className="relative ">
--- components/product-skeleton-card.tsx
+++ components/product-skeleton-card.tsx
@@ -5,7 +5,7 @@ export function SkeletonCard() {
     <div className="m-2">
       <div className="col-span-1 cursor-pointer group">
         <div className=" flex flex-col space-y-3">
-          <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+          <Skeleton className="h-[225px] rounded-xl" />
           <div className="space-y-2">
             <Skeleton className="h-4 w-[250px]" />
             <Skeleton className="h-4 w-[200px]" />
