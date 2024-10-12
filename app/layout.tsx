553b8d534f1de7a12bb7ad65766149f112045918
aminmusah <ameenmusah77@gmail.com>
2024-10-12 11:34:40 +0100

    fix clerk development mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html lang="en">
         <body className={nunito.className}>
           <div className="min-h-dvh grid grid-rows-[auto,1fr,auto] grid-cols-[minmax(0,1fr)]">
